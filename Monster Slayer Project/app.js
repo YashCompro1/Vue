18ad719f9d0b21ed247ca2dfcd45902e73774892
YashCompro1 <yash.aggarwal@comprotechnologies.com>
2024-09-16 18:22:03 +0530

    minor refactor. game over screen

--- Monster Slayer Project/app.js
+++ Monster Slayer Project/app.js
@@ -2,29 +2,57 @@ function generateRandomValue(low = 1, high = 10) {
 	return Math.floor(Math.random() * (high - low) + low);
 }
 
+function getWinner(playerHealth, monsterHealth) {
+	if (playerHealth <= 0 && monsterHealth <= 0) {
+		return "draw";
+	} else if (playerHealth <= 0) {
+		return "monster";
+	} else if (monsterHealth <= 0) {
+		return "player";
+	}
+
+	return null;
+}
+
 Vue.createApp({
 	data() {
 		return {
 			playerHealth: 100,
 			monsterHealth: 100,
 			attackCounter: 0,
+			winner: null,
 		};
 	},
 	computed: {},
+	watch: {
+		playerHealth() {
+			this.winner = getWinner(this.playerHealth, this.monsterHealth);
+		},
+		monsterHealth() {
+			this.winner = getWinner(this.playerHealth, this.monsterHealth);
+		},
+	},
 	methods: {
+		attackPlayer(low, high) {
+			this.playerHealth -= generateRandomValue(low, high);
+		},
+		attackMonster(low, high) {
+			this.monsterHealth -= generateRandomValue(low, high);
+		},
 		handleAttack() {
-			this.monsterHealth = Math.max(0, this.monsterHealth - generateRandomValue(5, 10));
-			this.playerHealth = Math.max(0, this.playerHealth - generateRandomValue(8, 16));
+			this.attackPlayer(8, 16);
+			this.attackMonster(5, 10);
 			this.attackCounter++;
 		},
 		handleSpecialAttack() {
-			this.monsterHealth = Math.max(0, this.monsterHealth - generateRandomValue(5, 10));
-			this.playerHealth = Math.max(0, this.playerHealth - generateRandomValue(15, 30));
+			this.attackPlayer(8, 16);
+			this.attackMonster(15, 30);
 			this.attackCounter = 0;
 		},
 		healPlayer() {
-			this.playerHealth = Math.max(0, this.playerHealth - generateRandomValue(8, 16));
 			this.playerHealth = Math.min(100, this.playerHealth + generateRandomValue(15, 25));
+			this.attackPlayer(8, 16);
+			this.attackCounter++;
 		},
 	},
 }).mount("#game");
