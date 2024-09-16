ac7e38c1729d043c5c10ccb1fcc5e50311f42d51
YashCompro1 <yash.aggarwal@comprotechnologies.com>
2024-09-16 17:52:09 +0530

    special attack

--- Monster Slayer Project/app.js
+++ Monster Slayer Project/app.js
@@ -1,18 +1,26 @@
+function generateAttackDamage(low = 1, high = 10) {
+	return Math.floor(Math.random() * (high - low) + low);
+}
+
 Vue.createApp({
 	data() {
 		return {
 			playerHealth: 100,
 			monsterHealth: 100,
+			attackCounter: 0,
 		};
 	},
 	computed: {},
 	methods: {
 		handleAttack() {
-			this.playerHealth -= this.generateAttackDamage(1, 5);
-			this.monsterHealth -= this.generateAttackDamage(1, 5);
+			this.monsterHealth = Math.max(0, this.monsterHealth - generateAttackDamage(5, 10));
+			this.playerHealth = Math.max(0, this.playerHealth - generateAttackDamage(8, 16));
+			this.attackCounter++;
 		},
-		generateAttackDamage(low = 1, high = 10) {
-			return ((Math.random() * 100) % high) + low;
+		handleSpecialAttack() {
+			this.monsterHealth = Math.max(0, this.monsterHealth - generateAttackDamage(5, 10));
+			this.playerHealth = Math.max(0, this.playerHealth - generateAttackDamage(15, 30));
+			this.attackCounter = 0;
 		},
 	},
 }).mount("#game");
