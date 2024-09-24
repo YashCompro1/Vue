2fb2572c28e62fbfad60d43dafd1be19941fea0c
YashCompro1 <yash.aggarwal@comprotechnologies.com>
2024-09-24 16:08:57 +0530

    teleport

---
+++ vue-cli/componentsContd/src/components/ErrorAlert.vue
@@ -0,0 +1,29 @@
+<template>
+    <dialog>
+        <header>
+            <slot name="header"></slot>
+        </header>
+        <slot></slot>
+    </dialog>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+dialog {
+    display: block;
+    position: fixed;
+    top: 20vh;
+    left: 50%;
+    width: 50%;
+    transform: translate(-50%);
+    background-color: white;
+    box-shadow: 0 2px 8px rgba(0, 0, 0, 0.26);
+    height: fit-content;
+    padding: 8px;
+}
+</style>
