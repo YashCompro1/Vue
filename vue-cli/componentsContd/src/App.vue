2261855cacc99d45b34775b9eff3b25384bd1396
YashCompro1 <yash.aggarwal@comprotechnologies.com>
2024-09-24 14:02:43 +0530

    scoped slots

--- vue-cli/componentsContd/src/App.vue
+++ vue-cli/componentsContd/src/App.vue
@@ -3,18 +3,26 @@
     <the-header></the-header>
     <badge-list></badge-list>
     <user-info :full-name="activeUser.name" :info-text="activeUser.description" :role="activeUser.role"></user-info>
+    <course-goals>
+      <template #default="slotProps">
+        <h2>{{ slotProps.data }}</h2>
+        <h3>{{ slotProps.newBind.data }}</h3>
+      </template>
+    </course-goals>
   </div>
 </template>
 
 <script>
 
 import BadgeList from './components/BadgeList.vue';
+import CourseGoals from './components/CourseGoals.vue';
 import TheHeader from './components/TheHeader.vue';
 
 export default {
   components: {
     'the-header': TheHeader,
-    'badge-list': BadgeList
+    'badge-list': BadgeList,
+    'course-goals': CourseGoals
   },
   data() {
     return {
---
+++ vue-cli/componentsContd/src/components/CourseGoals.vue
@@ -0,0 +1,17 @@
+<template>
+    <ul>
+        <li v-for="(goal, index) in goals" :key="goal">
+            <slot :data="goal" :newBind="{ data: 'value ' + index }"></slot>
+        </li>
+    </ul>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            goals: ['Goal 1', 'Goal 2']
+        }
+    }
+}
+</script>
