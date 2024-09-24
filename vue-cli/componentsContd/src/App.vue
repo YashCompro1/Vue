53eacfe57582505a827fdfdd1bf3b7afd7d72b30
YashCompro1 <yash.aggarwal@comprotechnologies.com>
2024-09-24 12:37:45 +0530

    local components

--- vue-cli/componentsContd/src/App.vue
+++ vue-cli/componentsContd/src/App.vue
@@ -7,7 +7,15 @@
 </template>
 
 <script>
+
+import BadgeList from './components/BadgeList.vue';
+import TheHeader from './components/TheHeader.vue';
+
 export default {
+  components: {
+    'the-header': TheHeader,
+    'badge-list': BadgeList
+  },
   data() {
     return {
       activeUser: {
